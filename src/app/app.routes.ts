b37ac7caf7dd01f2dde1ff28bb91a109f384ffa8
Alessio Valastro <alessio@MacBook-Air-di-Alessio.local>
2024-10-29 13:54:49 +0100

    navbar, login e signup. keep it up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'accedi',
+    component: LoginComponent,
+  },
+  { path: 'registrati', component: SignupComponent },
+  {path: '', component: HomeComponent}
+];
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from "./menu/menu.component";
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MenuComponent, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
