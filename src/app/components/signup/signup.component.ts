d17fa1fea4016ca15f1cd92024b88571d312a19e
Alessio Valastro <alessio@MacBook-Air-di-Alessio.local>
2024-11-03 16:39:08 +0100

    validazione form lato client

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-  onSubmit() {}
+  formData = new FormGroup({
+    name: new FormControl('', Validators.required),
+    surname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  getErrorMessage(fieldName: string): string {
+    const control = this.formData.get(fieldName);
+    if (control?.hasError('required')) {
+      return 'Questo campo è obbligatorio';
+    }
+    if (control?.hasError('minlength')) {
+      return `Lunghezza minima: ${control.errors?.['minlength'].requiredLength} caratteri`;
+    }
+    if (control?.hasError('email')) {
+      return 'Formato email non valido';
+    }
+    return '';
+  }
+
+  onSubmit() {
+    console.log(this.formData.value);
+  }
 }
